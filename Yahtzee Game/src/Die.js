74e93bdc93a8590928cc76f56acfadd4a9e96a5a
C-Fylachtos <talismannnngr@gmail.com>
2020-12-13 20:07:17 +0200

    Yahtzee game with React!

---
+++ Yahtzee Game/src/Die.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+
+
+
+class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one" , "two" , "three" , "four" , "five" , "six"]
+  }
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.props.handleClick(this.props.idx);
+  }
+
+  render() {
+    const {numberWords , locked , val , disabled , rolling } = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x`;
+    if(locked) classes += " Die-locked";
+    if(!locked && rolling) classes += " Die-rolling";
+    return (
+      <i
+        className={classes}
+        disabled={disabled}
+        onClick={this.handleClick}
+      >
+      </i>
+    );
+  }
+}
+
+export default Die;
